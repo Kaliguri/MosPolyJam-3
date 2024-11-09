a7a0e287187c2fc1a76d5e75ced6e820b82994f5
Gaida <kaliguri@bk.ru>
2024-11-10 00:03:49 +0300

    текст для тренировок

--- Assets/_Project/Script/Training Manager.cs
+++ Assets/_Project/Script/Training Manager.cs
@@ -1,16 +1,51 @@
+using System;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class TrainingManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Title("Text")]
+    public List<TrainingText> trainingTextList;
+
+    [Title("GameObject Reference")]
+    [SerializeField] TextMeshProUGUI tooltipHeader;
+    [SerializeField] TextMeshProUGUI tooltipText;
+
+    [ReadOnly] int currentText = -1;
+
+    public static UnityEvent TrainingPartComplete = new();
+    public static void SendTrainingPartComplete() {  TrainingPartComplete.Invoke(); }
+
+    static public TrainingManager instance;
+    void Awake()
     {
-        
+        instance = this;
+        TrainingPartComplete.AddListener(NextPart);
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        NextPart();
     }
+
+    public void NextPart()
+    {
+        currentText++;
+
+        //tooltipHeader.text = trainingTextList[currentText].Header;
+        tooltipText.text = trainingTextList[currentText].Text;
+
+    }
+
+}
+
+
+[Serializable]
+public class TrainingText
+{
+    //[TextArea] public string Header;
+    [TextArea] public string Text;
 }
