ff4ae9a0feed540203002e7650d72211976e5ed4
Gaida <kaliguri@bk.ru>
2024-11-09 19:57:04 +0300

    Работа над уровнем

--- Assets/_Project/Script/Animator Extension.cs
+++ Assets/_Project/Script/Animator Extension.cs
@@ -1,9 +1,10 @@
+using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class AnimatorExtension : MonoBehaviour
 {
-    public bool attackPrepared = false;
+    [ReadOnly] public bool attackPrepared = false;
 
     public static UnityEvent AttackPrepared = new();
     public void SendAttackPrepared() {  AttackPrepared.Invoke(); }
